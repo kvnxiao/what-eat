8036e376d45f88302416c5da747498e21ad4f54b
Kevin Xiao <kxiao@lyft.com>
2020-08-19 20:54:01 -0400

    Implement a search-style landing page

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,70 @@
 <template lang="pug">
 #app
-  #nav
-    router-link(to="/") Home
-    |  | 
-    router-link(to="/about") About
-  router-view
+  section.hero.is-primary.is-fullheight
+    .hero-body
+      .container
+        .header
+          h1.title.is-size-1 What Eat
+          h2.subtitle.is-size-3.is-italic Your #1 restaurant aggregator
+        .search-bar
+          .field.has-addons
+            .control.has-icons-left.search
+              input.input.is-medium.is-rounded(type="text", placeholder="I'm feeling...")
+              span.icon.is-left
+                fa(icon="utensils")
+            .control.location
+              button.button.is-medium
+                span.icon
+                  fa(icon="sliders-h")
+                span Location
+            .control
+              button.button.is-medium.is-rounded.is-danger
+                span.icon
+                  fa(icon="search")
+                span Search
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+$primary: #ff7c03
+$danger: #d32323
 
-#nav {
-  padding: 30px;
+@import "~bulma/bulma"
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&display=swap")
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+#app
+  font-family: 'Nunito Sans', Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+
+section.hero.is-primary
+  background-color: initial
+  background-image: linear-gradient(rgba(255, 124, 3, 0.75), rgba(255, 124, 3, 0.75)), url(https://s3-media0.fl.yelpcdn.com/assets/srv0/yelp_large_assets/1629eec0e96d/assets/img/home/hero_photos/3oz4uflrjm1Uftg_R689PA.jpg)
+
+#nav
+  padding: 30px
+
+  a
+    font-weight: bold
+    color: #2c3e50
+    &.router-link-exact-active
+      color: #42b983
+
+.search-bar
+  margin-top: 5rem
+  width: 100%
+
+  .field
+    width: 100%
+    justify-content: center
+
+  @include tablet
+    .control.search
+      width: 50%
+
+.location
+  .button
+    background-color: #ffdddd
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faUtensils, faSearch, faSlidersH } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
 Vue.config.productionTip = false
+
+library.add(faUtensils, faSearch, faSlidersH)
+
+Vue.component("fa", FontAwesomeIcon)
 
 new Vue({
   router,
